d42cfb6371f7d7b604f1b9cf25f9cc2c63da95c6
Rafaël Gonzalez <rafaelgonzalez@users.noreply.github.com>
2014-10-04 19:13:29 +1000

    Basic CLI with no error handling

--- lib/loco_bot.rb
+++ lib/loco_bot.rb
@@ -1,3 +1,4 @@
+require 'loco_bot/cli'
 require 'loco_bot/robot'
 require 'loco_bot/table'
 require 'loco_bot/version'
---
+++ lib/loco_bot/cli.rb
@@ -0,0 +1,56 @@
+module LocoBot
+  class CLI
+    attr_reader :robot, :table
+
+    def initialize
+      @robot = Robot.new
+      @table = Table.new
+    end
+
+    def input!(input)
+      instance_eval(input.downcase)
+    end
+
+    private
+
+    def place(x, y, direction)
+      robot.place(table, x, y, direction)
+    end
+
+    def right
+      robot.turn_right
+    end
+
+    def left
+      robot.turn_left
+    end
+
+    def move
+      robot.move
+    end
+
+    def report
+      report = robot.report
+
+      unless report.empty?
+        puts "#{report[:x]},#{report[:y]},#{report[:direction].label}"
+      end
+    end
+
+    def north
+      LocoBot::Robot::Direction::North
+    end
+
+    def south
+      LocoBot::Robot::Direction::South
+    end
+
+    def east
+      LocoBot::Robot::Direction::East
+    end
+
+    def west
+      LocoBot::Robot::Direction::West
+    end
+  end
+end
